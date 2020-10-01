ca473c2a5f1e09dfd25222eb53cc935f78b46926
Mary Baker <meb601@gmail.com>
2020-10-01 14:02:01 -0400

    Update greeting message again to include environment

--- api/server.js
+++ api/server.js
@@ -3,12 +3,11 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({
-      hello: "Web 34 from the web!",
-      goodbye: "It's 2pm, lecture is over.",
-    });
+  res.status(200).json({
+    hello: "Web 34 from the web!",
+    goodbye: "It's 2pm, lecture is over.",
+    environment: NODE_ENV,
+  });
 });
 
 module.exports = server;
