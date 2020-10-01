455fb3e6aa64216835a852ce7575a55668309f9f
Mary Baker <meb601@gmail.com>
2020-10-01 13:58:03 -0400

    Update greeting message again to include goodbye

--- api/server.js
+++ api/server.js
@@ -3,7 +3,12 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ hello: "Web 34 from the web!" });
+  res
+    .status(200)
+    .json({
+      hello: "Web 34 from the web!",
+      goodbye: "It's 2pm, lecture is over.",
+    });
 });
 
 module.exports = server;
