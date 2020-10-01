6c53414f32ac4a18e67569e8a174d8cfd765723e
Mary Baker <meb601@gmail.com>
2020-10-01 13:47:25 -0400

    Update greeting message

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const server = express();
 
 server.get("/", (req, res) => {
-  res.status(200).json({ hello: "Web 34" });
+  res.status(200).json({ hello: "Web 34 from the web!" });
 });
 
 module.exports = server;
